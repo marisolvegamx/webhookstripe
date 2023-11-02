88af878b2ca9df7ba9a7fa66300783f1ed4db623
marisolvegamx <40066757+marisolvegamx@users.noreply.github.com>
2023-11-02 16:55:25 -0600

    Add files via upload

---
+++ app/lib/Constantes.php
@@ -0,0 +1,20 @@
+<?php
+
+class Constantes {
+
+	/* LOCAL */
+    //public const WSDL 		= 'http://localhost:8081/orthoImagenDigitalWeb/services/OIDService?wsdl';
+	//public const FAC_WSDL 	= 'http://localhost:8081/orthoImagenDigitalWeb/services/OIDFacturacionService?wsdl';
+	
+	/* QA */
+    public const WSDL 			= 'http://18.189.141.62:8080/orthoImagenDigitalWeb/services/OIDService?wsdl';
+	public const FAC_WSDL 		= 'http://18.189.141.62:8080/orthoImagenDigitalWeb/services/OIDFacturacionService?wsdl';
+
+	/* PRODUCCION */
+	//public const WSDL = 'http://34.94.158.4:8080/orthoImagenDigitalWeb/services/OIDService?wsdl';	
+	//public const FAC_WSDL 	= 'http://34.94.158.4:8080/orthoImagenDigitalWeb/services/OIDFacturacionService?wsdl';
+	
+	
+}
+?>
+ 
