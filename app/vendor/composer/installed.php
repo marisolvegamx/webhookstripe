f03a76d7612d527ce7a9d8eb912ee299770310f8
marisolvegamx <40066757+marisolvegamx@users.noreply.github.com>
2023-11-02 16:50:35 -0600

    Add files via upload

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v8.12.0',
+      'version' => '8.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1b7b1ea75c744b8a9f55a0942112f8e822a6a482',
+    ),
+  ),
+);
