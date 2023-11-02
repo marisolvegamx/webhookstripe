a83585d757d36a8ccfe5c87f5fa2bda3a02bf194
marisolvegamx <40066757+marisolvegamx@users.noreply.github.com>
2023-11-02 16:30:57 -0600

    Add files via upload

---
+++ secrets_1.php
@@ -0,0 +1,7 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+$stripeSecretKey = 'sk_test_51HBvroG7NsTelwXI5Pnl81APDXCGnFslUCZxyrFNDmh4G6bjaVxMNicfm3kYOHq1hhtWuK8TNzwRbCvYFzyxL02N00ITbcDxOc';
+$endpoint_secret = 'whsec_59a62f73ffd22fcc374770b10e18933818a81dc9cd51157e0ca3484033b0a697';
+$stripepk="pk_test_51HBvroG7NsTelwXIVVtr7YrL5uZYRpej68vNMHPhN7KHrL7TJROFyXvVwOpqTHXXTC9RsSXLxNkgqEoWaOMU117E00JLSkcIkw";
---
+++ secrets_2.php
@@ -0,0 +1,10 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+$stripeSecretKey = 'sk_test_51NnRgkHkYNtt2qLfkdawcL2Dllo9ZBwpiWhewwosxho6Yf3csFK73ErXvWCtdKGe3EeQ92cWKzNNYB5bqan7XN8900KmntHJCb';
+$endpoint_secret = 'whsec_59a62f73ffd22fcc374770b10e18933818a81dc9cd51157e0ca3484033b0a697';
+$stripepk="pk_test_51NnRgkHkYNtt2qLfT9MynAwnkIeIQnKt6MqeSanw1zr96nSkbvFwMCj7eRQRF2jKLx061laKYkxV74WNftsdneNe004nyAjkmE";
